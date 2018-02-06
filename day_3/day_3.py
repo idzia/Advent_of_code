4dcc60c3b3f6495929d3c12d6419b08cd49db644
idzia <ania.idzi@gmail.com>
2018-02-06 23:36:58 +0100

    Add solution from 3 day

---
+++ day_3/day_3.py
@@ -0,0 +1,55 @@
+"""
+Each square on the grid is allocated in a spiral pattern starting at a location marked 1 and then counting up while spiraling outward. For example, the first few squares are allocated like this:
+
+17  16  15  14  13
+18   5   4   3  12
+19   6   1   2  11
+20   7   8   9  10
+21  22  23---> ...
+While this is very space-efficient (no squares are skipped), requested data must be carried back to square 1 (the location of the only access port for this memory system) by programs that can only move up, down, left, or right. They always take the shortest path: the Manhattan Distance between the location of the data and square 1.
+
+For example:
+
+Data from square 1 is carried 0 steps, since it's at the access port.
+Data from square 12 is carried 3 steps, such as: down, left, left.
+Data from square 23 is carried only 2 steps: up twice.
+"""
+import math
+
+def get_square_number(digit):
+
+    square_number = math.floor(math.ceil(math.sqrt(digit))/2)
+    return square_number
+
+digit = input("Please enter a number: ")
+digit = int(digit)
+square_number = get_square_number(digit)
+
+highest_number_in_square = (2 * square_number + 1)**2
+
+number_in_corner4 = highest_number_in_square
+number_in_corner3 = number_in_corner4 - (2 * square_number)
+number_in_corner2 = number_in_corner3 - (2 * square_number)
+number_in_corner1 = number_in_corner2 - (2 * square_number)
+
+middle_between_43 = number_in_corner4 - square_number
+middle_between_32 = number_in_corner3 - square_number
+middle_between_21 = number_in_corner2 - square_number
+middle_between_14 = number_in_corner1 - square_number
+
+if digit < number_in_corner4 and digit > number_in_corner3:
+    horizont_step = math.fabs(middle_between_43 - digit)
+
+elif digit < number_in_corner3 and digit > number_in_corner2:
+    horizont_step = math.fabs(middle_between_32 - digit)
+
+elif digit < number_in_corner2 and digit > number_in_corner1:
+    horizont_step = math.fabs(middle_between_21 - digit)
+
+elif digit < number_in_corner1:
+    horizont_step = math.fabs(middle_between_14 - digit)
+
+vertical_step = square_number
+steps = int(horizont_step + vertical_step)
+
+print(steps)
